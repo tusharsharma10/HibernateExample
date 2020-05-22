a0c084bd976f4183434ecaf20aa0a6486497203c
lenovo <sharmatushar759@gmail.com>
2020-05-22 12:37:49 +0530

    @ElementCollection mapping collection of value types

--- src/main/java/com/learn/hib/controller/EmployeeController.java
+++ src/main/java/com/learn/hib/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.learn.hib.controller;
 
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.hib.entity.Address;
 import com.learn.hib.entity.Employee;
 import com.learn.hib.entity.EmployeeStatus;
 import com.learn.hib.repo.EmployeeRepository;
+
 
 @RestController
 public class EmployeeController {
@@ -19,8 +24,23 @@ public class EmployeeController {
 		
 		Address a1 = new  Address("Bijolia", "Rajasthan", "India", 1000000);
 		
-		Employee e1 = new Employee(2, "Rajshekhran", EmployeeStatus.CONTRACT, a1);
+		Employee e1 = new Employee(3, "Rajshekhran", EmployeeStatus.CONTRACT, a1);
+		e1.addToNicknames("Raju");
+		empRepo.save(e1);
 		
+	}
+	
+	
+	@PutMapping("/update/employee")
+	public void updateEmployee() {
+		
+		Optional<Employee> f1 = empRepo.findById(2);
+		
+		Employee e1 = f1.get();
+		
+		e1.addToNicknames("Raju");
+		e1.addToNicknames("Rajan");
+		e1.addToNicknames("Shekar");
 		empRepo.save(e1);
 		
 	}
--- src/main/java/com/learn/hib/entity/Employee.java
+++ src/main/java/com/learn/hib/entity/Employee.java
@@ -1,10 +1,17 @@
 package com.learn.hib.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 
 @Entity
 public class Employee {
@@ -20,11 +27,26 @@ public class Employee {
 	@Embedded
 	private Address address;
 	
+	@ElementCollection
+	@CollectionTable(name="nickname", joinColumns = @JoinColumn(name="emp_id"))
+	@Column(name="nickname")
+	private Set<String> nicknames = new HashSet<String>();
 	
 	
 	
-	
-	
+	public Set<String> getNicknames() {
+		return nicknames;
+	}
+
+
+
+	public void addToNicknames(String nickname) {
+		
+		this.nicknames.add(nickname);
+	}
+
+
+
 	public Employee() {
 	}
 	
